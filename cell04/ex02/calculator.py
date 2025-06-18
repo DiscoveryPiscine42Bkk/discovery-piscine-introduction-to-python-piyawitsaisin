9d484d042d0da2cc62cce27ba411d8f96012213f
piyawitsaisin <68030178@kmitl.ac.th>
2025-06-18 03:58:51 +0000

    python

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,10 @@
+num1 = int(input("Give me the first number: "))
+num1 = int(num1)
+num2 = int(input("Give me the second number: "))
+num2 = int(num2)
+print("Thank you!")
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
+print(f"{num1} / {num2} = {num1 / num2}")
+print(f"{num1} * {num2} = {num1 * num2}")
+
