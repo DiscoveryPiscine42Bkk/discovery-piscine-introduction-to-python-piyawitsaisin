f3126afa2138bcbfbd84519de1602231c732ef45
piyawitsaisin <68030178@kmitl.ac.th>
2025-06-19 13:33:58 +0000

    python

---
+++ cell05/ex11/count_it.py
@@ -0,0 +1,9 @@
+import sys
+
+if len(sys.argv) == 1:
+    print("none")
+else:
+    args = sys.argv[1:]  
+    print(f"parameters: {len(args)}")
+    for arg in args:
+        print(f"{arg}: {len(arg)}")
